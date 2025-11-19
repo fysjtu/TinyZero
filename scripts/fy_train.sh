9f5ecfa28b84bd6f54e360f39ff5805e5fb88979
fanyu <1661741101@qq.com>
2025-11-19 23:27:56 +0800

    tmp

--- scripts/fy_train.sh
+++ scripts/fy_train.sh
@@ -17,14 +17,14 @@ actor_rollout_ref.model.use_remove_padding=True \
 actor_rollout_ref.actor.use_dynamic_bsz=False \
 actor_rollout_ref.actor.optim.lr=1e-6 \
 actor_rollout_ref.actor.ppo_mini_batch_size=8 \
-actor_rollout_ref.actor.ppo_micro_batch_size=1 \
+actor_rollout_ref.actor.ppo_micro_batch_size=2 \
 actor_rollout_ref.rollout.log_prob_micro_batch_size=1 \
 actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
 actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
 actor_rollout_ref.ref.log_prob_micro_batch_size=1 \
 critic.optim.lr=1e-5 \
 critic.model.path=$BASE_MODEL \
-critic.ppo_micro_batch_size=1 \
+critic.ppo_micro_batch_size=2 \
 critic.ppo_mini_batch_size=8 \
 algorithm.kl_ctrl.kl_coef=0.001 \
 trainer.logger=['wandb'] \
@@ -37,4 +37,5 @@ trainer.test_freq=100 \
 trainer.project_name=TinyZero \
 trainer.experiment_name=$EXPERIMENT_NAME \
 trainer.total_epochs=15 2>&1 | tee verl_demo.log
+
 
