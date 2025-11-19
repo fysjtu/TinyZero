33704b2f17c0ff2c12c48e419acb978f9103040a
fanyu <1661741101@qq.com>
2025-11-20 00:25:58 +0800

    'tmp'

--- scripts/2train_tiny_zero.sh
+++ scripts/2train_tiny_zero.sh
@@ -1,9 +1,9 @@
-export N_GPUS=1
+export N_GPUS=2
 export BASE_MODEL=/root/.cache/modelscope/hub/models/Qwen/Qwen2.5-1.5B
 export DATA_DIR=data/
-export ROLLOUT_TP_SIZE=1
+export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=countdown-qwen2.5-0.5b
-export VLLM_ATTENTION_BACKEND=XFORMERS
+# export VLLM_ATTENTION_BACKEND=XFORMERS
 
 python3 -m verl.trainer.main_ppo \
 data.train_files=$DATA_DIR/train.parquet \
